d7a4b286851d6febd54d8ce633b98df2e369c352
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-15 10:57:52 +0700

    step 9a

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -1,18 +1,7 @@
 
 
-            //insert in between node
-            //step 8: locate polition for insertion
-            Node *current = START;
-            while (current->next !=NULL && current->next->noMhs < nim)
-            {
-                current = current->next;
-            }
-
-            if (current->next !=NULL && nim == current->next->noMhs)
-            {
-                cout << "\nDuplicate roll number not allowed" << endl;
-                return;
-            }
+            //step 9: insert between current and cerrent->next
+            newNode->next = current->next;//step 9a
 
         }
 }
