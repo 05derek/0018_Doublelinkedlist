3879d4f4053a55ac35e47833154be6488b58fd53
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-15 10:40:18 +0700

    step 3

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -1,7 +1,14 @@
 
 
-            //step 2: Assign value to the data field
-            newNode->noMhs = nim;
+            //step 3 Insert at beginning if list is empety or nim is smallest
+            if (START == NULL || nim <= START->noMhs)
+            {
+                if (START != NULL && nim == START->noMhs)
+                {
+                    cout << "\nDuplicate number not allowed" << endl;
+                    return;
+                }
+            }
 
 
         }
